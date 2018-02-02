64f82c6d5d809ec8eece319fbe4433d4b6d4b1ae
Matt Bond <matt+github@codefire.io>
2018-02-02 14:55:25 -0500

    add chruby to fish

--- fish/config.fish
+++ fish/config.fish
@@ -5,3 +5,7 @@ pyenv rehash
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
 status --is-interactive; and source (nodenv init -|psub)
+
+source /usr/local/share/chruby/chruby.fish
+
+source /usr/local/share/chruby/auto.fish
