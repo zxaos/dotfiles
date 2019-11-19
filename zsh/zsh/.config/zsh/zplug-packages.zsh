834add687e7613910b16e7df0d83b03150245704
Matt Bond <matt+git@codefire.io>
2019-11-19 14:22:10 -0500

    add zsh forgit plugin

--- zsh/zsh/.config/zsh/zplug-packages.zsh
+++ zsh/zsh/.config/zsh/zplug-packages.zsh
@@ -2,3 +2,7 @@ zplug 'zplug/zplug', hook-build:'zplug --self-manage'
 #zplug 'nojhan/liquidprompt', use:liquidprompt, from:github, as:theme
 #zplug 'denysdovhan/spaceship-prompt', use:spaceship.zsh, from:github, as:theme
 zplug romkatv/powerlevel10k, use:powerlevel10k.zsh-theme
+
+if (( $+commands[fzf] )); then
+  zplug 'wfxr/forgit'
+fi
