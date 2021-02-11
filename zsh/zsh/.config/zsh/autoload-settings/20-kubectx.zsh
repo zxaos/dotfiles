33ac3aa164d0337555125e93a7353fb569ee9d55
Matt Bond <matt+git@codefire.io>
2021-02-11 13:46:45 -0500

    unset kubectx on startup in a simpler way

--- zsh/zsh/.config/zsh/autoload-settings/20-kubectx.zsh
+++ zsh/zsh/.config/zsh/autoload-settings/20-kubectx.zsh
@@ -1,4 +1,2 @@
 # unset default kubectl list so that there is no default context
-export KUBECONFIG="$HOME/.kube/emptyconfig"
-alias kubectx='unset KUBECONFIG; /usr/local/bin/kubectx'
-alias kuc='kubectl config unset current-context'
+kubectx -u > /dev/null
