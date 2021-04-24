8b22dde5481d759b914a07f3c97f38b939eb934e
Matt Bond <matt+git@codefire.io>
2021-04-24 18:12:55 -0400

    change bat theme based on terminal luma

--- configs/zsh/config-zsh/autoload-settings/90-aliases.zsh
+++ configs/zsh/config-zsh/autoload-settings/90-aliases.zsh
@@ -6,9 +6,19 @@ alias kxu="kubectl config unset current-context"
 alias tf='terraform'
 alias tower='open $(git rev-parse --show-toplevel) -a Tower'
 
-if type "bat" > /dev/null; then
-  # bat uses default theme when the OS is dark, and light theme when it's light
-  alias cat="bat --theme=\"\$(defaults read -globalDomain AppleInterfaceStyle &> /dev/null && echo '' || echo 'Monokai Extended Light')\""
+if (( $+commands[bat] )); then
+  bat_light='Monokai Extended Light'
+  bat_dark=
+
+  if (( $+commands[defaults] )); then
+    # bat uses default theme when the OS is dark, and light theme when it's light
+    alias cat="bat --theme=\"\$(defaults read -globalDomain AppleInterfaceStyle &> /dev/null && echo '$bat_dark' || echo '$bat_light')\""
+    alias bat="bat --theme=\"\$(defaults read -globalDomain AppleInterfaceStyle &> /dev/null && echo '$bat_dark' || echo '$bat_light')\""
+  elif (( $+functions[term_bg_dark] )); then
+    # Not on macos? See if we've defined a function to get terminal luma and approximate it that way instead.
+    alias cat="bat --theme=\"\$( term_bg_dark && echo '$bat_dark' || echo '$bat_light')\""
+    alias bat="bat --theme=\"\$( term_bg_dark && echo '$bat_dark' || echo '$bat_light')\""
+  fi
 fi
 
 # Exa in long format with slash for dirs and * for executables
