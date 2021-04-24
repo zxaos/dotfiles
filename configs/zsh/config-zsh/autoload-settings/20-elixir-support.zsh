6fc0e067a6d05804f18b7299a70d7c910bd52243
Matt Bond <matt+git@codefire.io>
2021-04-24 18:12:35 -0400

    don't load elixir customizations if it isn't installed

--- configs/zsh/config-zsh/autoload-settings/20-elixir-support.zsh
+++ configs/zsh/config-zsh/autoload-settings/20-elixir-support.zsh
@@ -1,2 +1,4 @@
-# Enable iex shell history
-export ERL_AFLAGS="-kernel shell_history enabled"
+if (( $+commands[elixir] )); then
+  # Enable iex shell history
+  export ERL_AFLAGS="-kernel shell_history enabled"
+fi
