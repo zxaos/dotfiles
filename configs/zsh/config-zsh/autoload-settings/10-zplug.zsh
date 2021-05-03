1841b5367080654c7d0708fd0912897e2768d662
Matt Bond <matt+git@codefire.io>
2021-05-02 23:54:04 -0400

    quite zsh startup

--- configs/zsh/config-zsh/autoload-settings/10-zplug.zsh
+++ configs/zsh/config-zsh/autoload-settings/10-zplug.zsh
@@ -1,21 +1,29 @@
 export ZPLUG_HOME=${ZPLUG_HOME:=${XDG_DATA_HOME}/zsh/zplug}
 export ZPLUG_LOADFILE=${ZPLUG_LOADFILE:=${ZDOTDIR}/zplug-packages.zsh}
 
-if [[ -r ${ZPLUG_HOME}/init.zsh ]]; then
-	source ${ZPLUG_HOME}/init.zsh
+function (){
+  if [[ $ZDOTDEBUG ]]; then
+    local zplugverbose="--verbose"
+  else
+    local zplugverbose=""
+  fi
 
-	# Plugins - See packages.zsh
+  if [[ -r ${ZPLUG_HOME}/init.zsh ]]; then
+    source ${ZPLUG_HOME}/init.zsh
 
-	# Install plugins if there are plugins that have not been installed
-	if ! zplug check --verbose; then
-		printf "Install? [y/N]: "
-		if read -q; then
-			echo; zplug install
-		fi
-	fi
+    # Plugins - See packages.zsh
 
-	# Then, source plugins and add commands to $PATH
-	zplug load --verbose
-fi
+    # Install plugins if there are plugins that have not been installed
+    if ! zplug check $zplugverbose; then
+      printf "Install? [y/N]: "
+      if read -q; then
+        echo; zplug install
+      fi
+    fi
+
+    # Then, source plugins and add commands to $PATH
+    zplug load $zplugverbose
+  fi
+}
 
 # vim: set syntax=zsh :
--- configs/zsh/config-zsh/autoload-settings/20-kubectx.zsh
+++ configs/zsh/config-zsh/autoload-settings/20-kubectx.zsh
@@ -1,4 +1,4 @@
 # unset default kubectl list so that there is no default context
 if type "kubectx" > /dev/null; then
-  kubectx -u > /dev/null
+  kubectx -u > /dev/null 2>&1
 fi
