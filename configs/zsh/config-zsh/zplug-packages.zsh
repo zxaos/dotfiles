e2578088a8e2143bd7a43d6d66fe881250c8b497
Matt Bond <matt+git@codefire.io>
2021-12-02 16:00:36 -0500

    add fzf tab completion

--- configs/zsh/config-zsh/zplug-packages.zsh
+++ configs/zsh/config-zsh/zplug-packages.zsh
@@ -1,3 +1,9 @@
+zplug 'xPMo/zsh-ls-colors'
+
 if (( $+commands[fzf] )); then
   zplug 'wfxr/forgit'
+  # Load fzf-tab after compinit
+  zplug 'Aloxaf/fzf-tab', \
+    defer:2, \
+    hook-load:"source ${0:a:h}/plugin-settings/fzf-tab.zsh"
 fi
