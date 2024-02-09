ac18ae32ca9074417e300e4a5ded781fb56c6ba3
Matt Bond <matt+git@codefire.io>
2024-02-09 13:44:04 -0500

    split Brewfile into variants

--- setup/scripts/01-brew-bundle.sh
+++ setup/scripts/01-brew-bundle.sh
@@ -1,4 +1,23 @@
 #!/usr/bin/env bash
 
-cd $(dirname $0)/..
-brew bundle
+readonly prefs=.setupstyle.sh
+
+cd "$(dirname "$0")/.." || exit 1
+if [ -f "$prefs" ]; then
+    # shellcheck source=.setupstyle.sh
+    . "$prefs"
+fi
+
+if [ -z "$setupstyle" ]; then
+    read -r -p "Use which template for Brewfile? (home | work): " setupstyle
+    echo "setupstyle=${setupstyle}" > "$prefs"
+fi
+
+brewtarget="Brewfile-${setupstyle}"
+if [ -f "${brewtarget}" ]; then
+    brew bundle --file "${brewtarget}"
+else
+    rm .brewstyle.sh
+    echo "Error: template '${setupstyle}' was not found"
+    exit 1
+fi
