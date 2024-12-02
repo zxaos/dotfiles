4e899f060fea8842e4f309365e1fc0acab39eb22
Matt Bond <matt+git@codefire.io>
2024-12-02 10:40:34 -0500

    fix warp catpuccin urls

--- setup/scripts/20-warp-themes.sh
+++ setup/scripts/20-warp-themes.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 mkdir -p ~/.warp/themes
-curl --output-dir ~/.warp/themes -LO https://raw.githubusercontent.com/catppuccin/warp/main/dist/catppuccin_latte.yml
-curl --output-dir ~/.warp/themes -LO https://raw.githubusercontent.com/catppuccin/warp/main/dist/catppuccin_frappe.yml
-curl --output-dir ~/.warp/themes -LO https://raw.githubusercontent.com/catppuccin/warp/main/dist/catppuccin_macchiato.yml
-curl --output-dir ~/.warp/themes -LO https://raw.githubusercontent.com/catppuccin/warp/main/dist/catppuccin_mocha.yml
+curl --output-dir ~/.warp/themes/ -OL0 https://raw.githubusercontent.com/catppuccin/warp/refs/heads/main/themes/catppuccin_latte.yml
+curl --output-dir ~/.warp/themes/ -OL0 https://raw.githubusercontent.com/catppuccin/warp/refs/heads/main/themes/catppuccin_frappe.yml
+curl --output-dir ~/.warp/themes/ -OL0 https://raw.githubusercontent.com/catppuccin/warp/refs/heads/main/themes/catppuccin_frappe.yml
+curl --output-dir ~/.warp/themes/ -OL0 https://raw.githubusercontent.com/catppuccin/warp/refs/heads/main/themes/catppuccin_macchiato.yml
+curl --output-dir ~/.warp/themes/ -OL0 https://raw.githubusercontent.com/catppuccin/warp/refs/heads/main/themes/catppuccin_mocha.yml
